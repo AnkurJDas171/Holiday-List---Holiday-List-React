e651e34362a8ca8eafd35cdcedf05c844fb35f02
unknown <ankurjyotidas171@gmail.com>
2020-10-08 20:25:30 +0530

    test cases

--- src/components/App.js
+++ src/components/App.js
@@ -4,9 +4,19 @@ import '../styles/App.css';
 class App extends Component {
     render() {
 
+
+
+        //Goa(India),Amsterdam(Netherlands),New York(USA),Darjeeling(India),Tokyo(Japan),Lonavala(India)
+
+        const cities = [`Goa(India)`,`Amsterdam(Netherlands)`,`New York(USA)`,`Darjeeling(India)`,`Tokyo(Japan)`,`Lonavala(India)`];
+
+
         return(
            <>
+           <ol>
+        {cities.map((city,index) => <li key={"location"+{index}}>{city}</li>)}
 //write your code here
+            </ol>
 	</>
         )
     }
